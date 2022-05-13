163af43ed06338c3b6f328763d246279e759aae3
Joao Paulo <oliveirajpufrj@gmail.com>
2022-05-13 15:09:37 -0300

    feat: add jersey to cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,10 +2,14 @@ import db from "../db.js";
 
 export const postCart = async (req, res) => {
     const {name, price, img} = req.body;
-    
+    const { user } = res.locals
+
     try {
         console.log(req.body);
         const camisa = await db.collection("jerseys").findOne({name});
+        await db.collection("users").update({name: user.name},{$push: {
+            cart: {name, price, img}
+        }})
         
     } catch (error) {
         console.log("post cart", error);
--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { postCart } from "../controllers/cartController.js";
+import { validToken } from "../middlewares/validToken.js";
 
 const cartRouter = Router()
 
-cartRouter.post("/cart", postCart)
+cartRouter.post("/cart", validToken,postCart)
 export default cartRouter
