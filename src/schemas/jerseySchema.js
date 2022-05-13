54bb365ba101f3870b88a1b7fcb56e741aafbb81
Alyson Consuli <consulialyson@gmail.com>
2022-05-13 16:10:54 -0300

    feat: uptade jersey schema

--- src/schemas/jerseySchema.js
+++ src/schemas/jerseySchema.js
@@ -3,5 +3,6 @@ import joi from 'joi'
 export const jerseySchema = joi.object({
     name: joi.string().required(),
     img: joi.string().required(),
-    price: joi.number().required().positive()
+    price: joi.number().required().positive(),
+    type: joi.string().required().valid('nacional', 'internacional')
 })
