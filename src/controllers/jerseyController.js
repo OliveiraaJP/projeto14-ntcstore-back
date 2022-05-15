3bf05ea802a9821f86bc1debf412abaff0732ed2
Alyson Consuli <consulialyson@gmail.com>
2022-05-15 00:58:53 -0300

    fix: apagar id, e utilizar o _id

--- src/controllers/jerseyController.js
+++ src/controllers/jerseyController.js
@@ -11,11 +11,7 @@ export const getJerseys = async (req, res) => {
 
 export const postJersey = async (req, res) => {
     try {
-        const jerseys = await db.collection('jerseys').find({}).toArray()
-        await db.collection('jerseys').insertOne({
-            ...req.body,
-            id: jerseys.length ? Number(jerseys[jerseys.length - 1].id) + 1 : 0
-        })
+        await db.collection('jerseys').insertOne(req.body)
         res.sendStatus(201)
     } catch {
         res.sendStatus(500)
