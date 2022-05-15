607065c168f1ae2c4950f0fb4934b1418671d954
Alyson Consuli <consulialyson@gmail.com>
2022-05-15 17:39:48 -0300

    feat: autenticaçao do token com jwt

--- src/middlewares/validAdminTokenMiddleware.js
+++ src/middlewares/validAdminTokenMiddleware.js
@@ -1,10 +1,18 @@
 import db from "../db.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const validAdminToken = async (req, res, next) => {
     try {
         const { authorization } = req.headers
         const token = authorization?.replace('Bearer ', '').trim()
         if (!token) {
+            return res.sendStatus(401);
+        }
+        const secretKey = process.env.JWT_SECRET;
+        const data = jwt.verify(token, secretKey);
+        if (!data) {
             return res.sendStatus(401);
         }
         const session = await db.collection('adminSession').findOne({ token })
